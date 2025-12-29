daf5794fa85cccc2ff6d229322b9df67cab89f5d
Fernando Parra <f.parrav@uniandes.edu.co>
2025-12-29 11:23:17 -0500

    Add sonar improvements

--- src/files/b2.py
+++ src/files/b2.py
@@ -31,6 +31,6 @@ def b2_upload_file(local_file: str, file_name: str) -> str:
         local_file=local_file, file_name=file_name
     )
     download_url = api.get_download_url_for_fileid(uploaded_file.id_)
-    logger.debug(f"")
+    logger.debug("Upload completed successfully")
 
     return download_url
--- src/user/router.py
+++ src/user/router.py
@@ -1,10 +1,10 @@
 import logging
 
-from fastapi import APIRouter, HTTPException, status, Depends
+from fastapi import APIRouter, HTTPException, status
 
 from src.database.postgres import database, user_table
 from src.user.model import UserIn
-from src.auth.service import get_password_hash, get_user, authenticate_user, create_access_token
+from src.auth.service import get_password_hash, get_user
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -12,13 +12,13 @@ router = APIRouter()
 
 @router.post("/register", status_code=201)
 async def register(user: UserIn):
-    if await get_user(user.email):
+    if await get_user(str(user.email)):
         raise HTTPException(
             status_code=status.HTTP_400_BAD_REQUEST,
             detail="User already exists"
         )
     hashed_password = get_password_hash(user.password)
-    query = user_table.insert().values(email=user.email, password=hashed_password)
+    query = user_table.insert().values(email=str(user.email), password=hashed_password)
 
     logger.debug(query)
 
