df904824f6cf4101582e20f6da0049197248e3f6
Fernando Parra <f.parrav@uniandes.edu.co>
2025-12-30 15:19:10 -0500

    Add /me endpoint for Swagger

--- src/auth/router.py
+++ src/auth/router.py
@@ -1,16 +1,14 @@
 import logging
-
 from typing import Annotated
 from fastapi import APIRouter, Depends
 from fastapi.security import OAuth2PasswordRequestForm
-
 from src.auth.service import authenticate_user, create_access_token
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.post("/token")
+@router.post("/login")
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = await authenticate_user(form_data.username, form_data.password)
     access_token = create_access_token(user.email)
